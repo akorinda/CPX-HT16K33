a306c00b8691f08b7889e6b2a1fdeedb175680c8
akorinda <46490460+akorinda@users.noreply.github.com>
2026-07-21 23:31:24 -0400

    Return test.ts

---
+++ test.ts
@@ -0,0 +1,8 @@
+// Test file required if listed under testFiles in pxt.json
+alphaDisplay.initializeAlphanumericDisplay(112)
+alphaDisplay.setBrightness(15)
+alphaDisplay.showString("test")
+basic.pause(1000)
+alphaDisplay.showNumber(42)
+basic.pause(1000)
+alphaDisplay.clear()
